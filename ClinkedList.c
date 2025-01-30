801a739af789aa2f240f4bec7e604eabd8443c60
gulshan <mgulshan2003@gmail.com>
2025-01-30 22:02:56 +0530

    +header files.

---
+++ ClinkedList.c
@@ -0,0 +1,5 @@
+// Implementing Doubly Linked List - abstract data type.
+
+#include <stdio.h>      // for: input & output.
+#include <conio.h>      // for: getch();
+#include <stdlib.h>     // for: exit(), system();
